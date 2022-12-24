e12523fde2a5cdbc6ff4128b642312feec46c9d5
kyere7 <gyabaakyere@gmail.com>
2022-12-24 04:38:42 +0000

    added files

---
+++ sea_level_predictor.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import linregress
+
+def draw_plot():
+    # Read data from file
+    df = pd.read_csv("epa-sea-level.csv")
+    x = df["Year"]
+    y = df["CSIRO Adjusted Sea Level"]
+
+    # Create scatter plot
+
+
+    # Create first line of best fit
+
+
+    # Create second line of best fit
+
+
+    # Add labels and title
+
+    
+    # Save plot and return data for testing (DO NOT MODIFY)
+    plt.savefig('sea_level_plot.png')
+    return plt.gca()
